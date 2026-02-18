3449594166caa225bf13aa1d949591f6af108d49
JansQ-75 <janina7590@hotmail.com>
2026-02-18 18:57:49 +0200

    minor import fix

--- interfaces/http/controllers/getSessionController.ts
+++ interfaces/http/controllers/getSessionController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import getSessions from "../../../application/session/getSessions";
+import getSessions from "../../../application/session/GetGameSessions";
 
 export default (getSessionsUseCase: getSessions) =>
   async (req: Request, res: Response): Promise<void> => {
