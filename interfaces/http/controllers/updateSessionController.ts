6e33f3fa529f93bc92d29274b09047aac8869b39
satupa <satupalv@gmail.com>
2026-02-18 17:06:53 +0200

    Updated updateSession for better frontend use and added auth to addFriend

--- interfaces/http/controllers/updateSessionController.ts
+++ interfaces/http/controllers/updateSessionController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
-import createSessionUseCases from "../../../composition/session";
+import UpdateSession from "../../../application/session/updateSession";
 
-const { UpdateSession } = createSessionUseCases();
-
-export const updateSessionController = async (req: Request, res: Response) => {
-  try {
-    const sessionData = req.body;
-    await UpdateSession.execute(sessionData);
-    res.status(200).json({ message: "Session updated successfully" });
-  } catch (error) {
-    res.status(400).json({ error: (error as Error).message });
-  }
-};
+export const updateSessionController =
+  (updateSession: UpdateSession) =>
+  async (req: Request, res: Response) => {
+    try {
+      const sessionData = req.body;
+      await updateSession.execute(sessionData);
+      res.status(200).json({ message: "Session updated successfully" });
+    } catch (error) {
+      res.status(400).json({ error: (error as Error).message });
+    }
+  };
